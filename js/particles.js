0864ed7dc658d4d3efdbd57f44cd337d4917c330
superninjaX2 <lunguj665@gmail.com>
2024-06-29 20:20:00 +0200

    particles witjout color

--- js/particles.js
+++ js/particles.js
@@ -5,27 +5,70 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var x = 20, y = 100;
+const particles = [];
+const numParticles = 200;
 
-// Defining the draw function
-const draw = () => {
-    ctx.fillStyle = "#fff";
-    ctx.beginPath();
-    ctx.arc(x, y, 5, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.stroke();
-};
+class Particle {
+    constructor(x, y, vx, vy) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.radius = 5;
+    }
 
-const update = () => {
-    y -= 1;
-};
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
 
-// Calling the draw function
+    draw() {
+        ctx.fillStyle = "#fff";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    isOffscreen() {
+        return (
+            this.x < 0 || this.x > canvas.width ||
+            this.y < 0 || this.y > canvas.height
+        );
+    }
+}
+
+// Initialize particles
+for (let i = 0; i < numParticles; i++) {
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    let vx = (Math.random() - 0.5) * 2;
+    let vy = (Math.random() - 0.5) * 2;
+    particles.push(new Particle(x, y, vx, vy));
+}
+
 const loop = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    draw();
-    update();
+
+    for (let i = particles.length - 1; i >= 0; i--) {
+        let p = particles[i];
+        p.update();
+        p.draw();
+
+        // Remove particle if offscreen
+        if (p.isOffscreen()) {
+            particles.splice(i, 1);
+        }
+    }
+
     requestAnimationFrame(loop);
 };
 
 loop();
+//end of partcles
+//now begin the fading animation
+gsap.fromTo('#cvs',{opacity:0}, {
+    scrollTrigger: canvas, // start animation when ".box" enters the viewport
+    opacity:1,
+    duration:6,
+});
